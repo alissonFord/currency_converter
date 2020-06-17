f3548de9780fa2f11d411745bbc56b95db8a3ba6
alissonFord <alicjaszlachta@outlook.com>
2020-06-17 19:57:33 +0200

    basic converter added

--- converter.js
+++ converter.js
@@ -1,3 +1,10 @@
+const endpoint = 'https://api.exchangeratesapi.io/latest';
+const exchangeForm = document.querySelector('form');
+const selectFrom = document.querySelector(`[name="from_currency"]`);
+const selectTo =document.querySelector(`[name="to_currency"]`);
+const amountToExchange = document.querySelector(`[name="from_amount"]`);
+const exchangedAmount = document.querySelector('.to_amount');
+
 const currencies = {
     USD: 'United States Dollar',
     AUD: 'Australian Dollar',
@@ -32,3 +39,42 @@ const currencies = {
     ZAR: 'South African Rand',
     EUR: 'Euro',
   };
+
+
+  function generateOptions(options) {
+    return Object.entries(options).map(([currencySymbol, currencyName]) =>
+      `<option value="${currencySymbol}">${currencySymbol} - ${currencyName}</option>`
+    )
+    .join('');
+  }
+
+  selectFrom.innerHTML = generateOptions(currencies);
+  selectTo.innerHTML = generateOptions(currencies);
+
+  async function fetchExchangeRate(base) {
+    const query = `${endpoint}?base=${base}`;
+    const result = await fetch(query);
+    const rate = await result.json();
+    return rate;
+  }
+
+  async function countExchangeAmount(from, to, amount) {
+    const { rates } = await fetchExchangeRate(from);
+    const result = rates[to] * amount;
+    console.log(result);
+    return result;
+  }
+
+  async function handleInput(e) {
+    console.log(e.currentTarget);
+    console.log(e.target);
+    const exchange = await countExchangeAmount(
+      selectFrom.value,
+      selectTo.value,
+      amountToExchange.value
+    )
+    exchangedAmount.textContent = exchange;
+  }
+
+  exchangeForm.addEventListener('input', handleInput);
+
